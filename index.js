63da2b5978bfd3849ce82cba9020d00c069e5f09
web3 <web3@Devarajs-MacBook-Pro.local>
2022-02-02 17:46:34 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,69 @@
+
+//if(true)
+//{
+//    let name ="vinay";
+//    alert(name);
+//}   
+
+
+//let fname = "vinay";
+//let lname = "Prakash";
+//let age = prompt("enter your age ");
+
+//let result = `${fname} ${lname} age is ${age}`;
+//alert(result);
+
+//Default parameters
+
+//function welcome(user='vinay',message='you der')
+//{
+  //  alert(`hello ${user} ${message}`)
+//}
+//welcome();
+
+// Arrow functions
+
+// function greeting(message)
+// {
+//    alert(`Hello ${message}`);
+// }
+
+// greeting('Everyone');
+
+// Below function is for single argument
+//let greeting = (message) => alert(`Hello ${message}`);
+
+//greeting('Everyone');
+
+//below function for multiple arguments
+
+//let greeting = (message,wish) => alert(`Hello ${message} ${wish}`);
+
+//greeting('Everyone', 'good morning');
+
+// let createBlog = (title, body) => {
+//     if(!title)
+//     { return alert("hi");}
+
+
+//     if(!body)
+//     { return alert(`Body is required`);}
+
+//     return alert(`${title} ${body}`);
+// };
+
+// createBlog('HELLO','BODY');
+
+// intro to this
+
+// let banglore = {
+//     //property
+//     cities : ['basavangudi','Nr colony','TR nagar'],
+// //method
+//     printcities : function()   {
+//         setTimeout(() => console.log(this.cities),3000);
+//     }
+
+// }
+// banglore.printcities(); 
+
